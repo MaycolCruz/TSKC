5a7aa411951c8ef1a6a96c1fcbce7494866c140a
GianAle1 <gian.alejandro@unmsm.edu.pe>
2023-05-28 17:38:48 -0500

    Update BasedeDatos.sql

--- Desarrollo/SSPP/Codigo/BasedeDatos.sql
+++ Desarrollo/SSPP/Codigo/BasedeDatos.sql
@@ -70,20 +70,24 @@ SELECT * FROM Curso
 SELECT * FROM prision
 SELECT * FROM Celda
 SELECT * FROM Conducta
-SELECT * FROM Calificacion_cond
+SELECT * FROM recluso
 iNSERT INTO prision VALUES (1,'Santa Monica','Chorrillos cuadra 5',160) 
 iNSERT INTO Celda VALUES (1,322,5,1) 
-iNSERT INTO Recluso VALUES (1,'Jose','Romero','01/12/1999',1,1,1,1)
-iNSERT INTO Recluso VALUES (2,'Marco','Benites','01/12/2000',1,1,1,1) 
-iNSERT INTO Recluso VALUES (4,'Romero','xas','01/12/2000',2,1,1,1) 
+-- Inserta nuevos registros en la tabla Recluso
+INSERT INTO Recluso VALUES (1,'Jose','Romero','01/12/1999',1,1,1,1)
+INSERT INTO Recluso VALUES (2,'Marco','Benites','01/12/2000',1,1,1,1)
+INSERT INTO Recluso VALUES (3,'Juan','xas','01/12/2000',2,1,1,1)
+select * from Matricula
+-- Inserta registros en la tabla Matricula para asignar cursos a los reclusos
+INSERT INTO Matricula VALUES (1,1,1)
+INSERT INTO Matricula VALUES (2,2,2)
+INSERT INTO Matricula VALUES (3,3,3)
 iNSERT INTO curso VALUES (1,'Mecanica')
 iNSERT INTO curso VALUES (2,'Soldadura')
 iNSERT INTO curso VALUES (3,'Jardineria')
 iNSERT INTO curso VALUES (4,'Informatica')
 iNSERT INTO curso VALUES (5,'Arte y manualidades')
-iNSERT INTO Matricula VALUES (1,1,1)
-iNSERT INTO Matricula VALUES (2,2,2)
-iNSERT INTO Matricula VALUES (3,3,3)
+
 iNSERT INTO Conducta VALUES (1,'Buena conducta','Cumple con las reglas y normas de la prisión')
 iNSERT INTO Conducta VALUES (2,'Conducta regular','Algunas infracciones menores')
 iNSERT INTO Conducta VALUES (3,'Mala conducta','Infracciones graves')
@@ -97,12 +101,20 @@ iNSERT INTO Crimen VALUES (4,'Trafico de drogas','La producción, distribución, v
 iNSERT INTO Crimen VALUES (5,'Fraude','Actividades engañosas realizadas para obtener beneficios financieros o personales de manera deshonesta, como estafas o falsificación de documentos.')
 
 
-SELECT Recluso.Nombre, Recluso.Apellido, Recluso.Fecha_nacimiento,Crimen.Nombre, Prision.Nombre, Celda.Numero, Curso.Nombre, Conducta.Nombre
-FROM Recluso
-JOIN Celda ON Recluso.Cod_celda = Celda.Cod_celda
-JOIN Prision ON Celda.Cod_prision = Prision.Cod_prision
-JOIN Matricula ON Recluso.Cod_recluso = Matricula.Cod_recluso
-JOIN Curso ON Matricula.Cod_curso = Curso.Cod_curso
-JOIN Calificacion_cond ON Calificacion_cond.Cod_recluso = Recluso.Cod_recluso
-JOIN Conducta ON Conducta.Cod_conducta = Calificacion_cond.Cod_conducta
-JOIN Crimen ON Recluso.Cod_crimen=Crimen.Cod_crimen 
+CREATE FUNCTION ObtenerDatosRecluso()
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT Recluso.Nombre, Recluso.Apellido, Recluso.Fecha_nacimiento, Crimen.Nombre AS CrimenNombre, Prision.Nombre AS PrisionNombre, Celda.Numero AS CeldaNumero, Curso.Nombre AS CursoNombre, Conducta.Nombre AS ConductaNombre
+    FROM Recluso
+    JOIN Celda ON Recluso.Cod_celda = Celda.Cod_celda
+    JOIN Prision ON Celda.Cod_prision = Prision.Cod_prision
+    JOIN Matricula ON Recluso.Cod_recluso = Matricula.Cod_recluso
+    JOIN Curso ON Matricula.Cod_curso = Curso.Cod_curso
+    JOIN Calificacion_cond ON Calificacion_cond.Cod_recluso = Recluso.Cod_recluso
+    JOIN Conducta ON Conducta.Cod_conducta = Calificacion_cond.Cod_conducta
+    JOIN Crimen ON Recluso.Cod_crimen = Crimen.Cod_crimen
+);
+select * from ObtenerDatosRecluso()
+
